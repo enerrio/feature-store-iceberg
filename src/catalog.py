0e4d6ba04e873bb336c9d1c70b928d61501627be
enerrio <12959255+enerrio@users.noreply.github.com>
2025-09-01 22:04:38 -0700

    update catalog

--- src/catalog.py
+++ src/catalog.py
@@ -4,9 +4,10 @@ from pyiceberg.catalog import load_catalog
 def get_catalog():
     """Single source of truth for catalog configuration."""
     return load_catalog(
-        "local",
+        "rest",
         **{
             "type": "rest",
             "uri": "http://localhost:8181/",
+            "warehouse": "file:///tmp/warehouse",
         },
     )
